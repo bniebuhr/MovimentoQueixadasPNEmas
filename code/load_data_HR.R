8ac2b813cb4b757c4956335fe84b65ffbbb27b64
Bernardo Brandao Niebuhr Dos Santos <bernardo.brandao@slu.se>
2019-10-16 19:02:57 +0200

    added ssf for all individual, first version

--- code/load_data_HR.R
+++ code/load_data_HR.R
@@ -3,11 +3,11 @@
 #' author: Bernardo Niebuhr, Ennio Painkow, Ronaldo Morato - CENAP/ICMBio
 #' ---
 
-#' # Loading and organizing data
+#' # Loading and clean data
 #' 
-#' First of all, we'll load and organize data, to prepare them as input to the
+#' First of all, we load and organize data, to prepare them as input to the
 #' different movement analysis packages. We're working here with data from four
-#' white-lipped peccary individuals (from four different groups) from the Parque Nacional
+#' white-lipped peccary individuals (from four different groups) from Parque Nacional
 #' das Emmas, within the Brazilian Cerrado.
 #' 
 
@@ -36,6 +36,10 @@ install.load::install_load('move', 'amt', 'adehabitatLT', 'ctmm')
 
 #' ## Load data
 #' 
+#' Below we load the data for each of the animals (separated in different sheets), 
+#' transform dates and times into 
+#' `datetime` objects, rename columns with easier names, and keep only the ones which are
+#' relevant for these analyses. In the end, we combine all `data.frame`s in a single one.
 
 # Load Alto Forsmoso movement data
 mov.data.alto.formoso <- readr::read_csv('data/6010 PLT Alto Formoso/Pontos Planilha 6010 PLT Alto Formoso Consolidado.csv', skip = 23) %>% 
@@ -52,7 +56,7 @@ mov.data.alto.formoso <- readr::read_csv('data/6010 PLT Alto Formoso/Pontos Plan
 mov.data.alto.formoso
 
 # Load Olhos D'Agua Leste movement data
-mov.data.olhos_dagua_leste <- readr::read_csv("data/Olhos Leste/Pontos Planilha Olhos Leste Consolidado.csv", skip = 23) %>% 
+mov.data.olhos_dagua_leste <- readr::read_csv("data/Olhos Leste/data_log_olhos_leste.csv", skip = 22) %>% 
   dplyr::mutate(timestamp = lubridate::ymd_hms(`GPS Fix Time`),
                 name = "Olhos D'Agua Leste") %>% 
   dplyr::rename(fix_attempt = `GPS Fix Attempt`,
@@ -80,7 +84,7 @@ mov.data.olhos_dagua_oeste <- readr::read_csv("data/Olhos Oeste/Pontos Planilha 
 mov.data.olhos_dagua_oeste
 
 # Load Pontal movement data
-mov.data.pontal <- readr::read_csv("data/Pontal/Pontos Planilha Pontal Consolidado.csv", skip = 23) %>% 
+mov.data.pontal <- readr::read_csv("data/Pontal/pontal_atualizado_20191011.csv", skip = 23) %>% 
   dplyr::mutate(timestamp = lubridate::ymd_hms(`GPS Fix Time`),
                 name = "Pontal") %>% 
   dplyr::rename(fix_attempt = `GPS Fix Attempt`,
@@ -97,6 +101,15 @@ mov.data.pontal
 mov.data <- dplyr::bind_rows(mov.data.alto.formoso, mov.data.olhos_dagua_leste, mov.data.olhos_dagua_oeste, mov.data.pontal)
 
 #' ## Explore and filter data
+#' 
+#' Now we perform a general data cleaning. 
+#' We count the number of failed attempts to get data and remove them, remove 
+#' duplicate points, remove spikes (extreme ouliers, in a more or less manual way),
+#' plot the data (just to check if everything seems right), export them as a single 
+#' sheet, but also as Shapefile and Geopackage files, to be open directly in GIS
+#' software. We also standardize the starting date of the data - we consider only
+#' locations after April 8th 2019.
+#' 
 
 # Total number of fails in fix attempt
 table(mov.data$fix_attempt)
@@ -141,7 +154,7 @@ mov.data.clean <- mov.data.clean %>%
 # Remove outliers
 (names <- unique(mov.data.clean$name))
 out.af <- which(mov.data.clean$name == names[1] & mov.data.clean$latitude < -18.6)
-out.odal <- which(mov.data.clean$name == names[2] & mov.data.clean$longitude < -52.94)
+out.odal <- which(mov.data.clean$name == names[2] & (mov.data.clean$longitude < -52.94 | mov.data.clean$latitude < -18.34))
 
 mov.data.clean <- mov.data.clean[-c(out.af, out.odal),]
 
@@ -153,15 +166,22 @@ ggplot(data = mov.data.clean) +
 # Export as vector
 mov.data.clean %>% 
   sf::st_as_sf(coords = c('longitude', 'latitude'), crs = 4326) %>% # CRS(+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs)
-  sf::st_write(paste0('data/movement_data_all_individuals.shp'),
-               delete_dsn = TRUE)
+  sf::st_write('data/movement_data_all_individuals.shp', delete_dsn = TRUE)
 
 mov.data.clean %>% 
   sf::st_as_sf(coords = c('longitude', 'latitude'), crs = 4326) %>% # CRS(+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs)
   sf::st_write(paste0('data/movement_data_all_individuals.gpkg'),
                delete_dsn = TRUE)
 
-#' ## General information on the data
+mov.data.clean %>% 
+  readr::write_csv('data/movement_data_all_individuals.csv')
+
+#' # General information on monitoring time and movement parameters
+#' 
+#' In this section we transform data into a `track` object from the `amt` package and 
+#' plot information about the monitoring time (and exclude some sparse data from one of 
+#' the individuals). We also calculate and plot some basic movement parameters, such as 
+#' step lengths and turning angles, for individual and for different time frames.
 
 # amt
 mov.track <- mov.data.clean %>% 
@@ -194,10 +214,15 @@ g.mon <- ggplot() +
 g.mon
 
 #' **Should we consider the movement of 'Olhos D'Agua Oeste' only until the beginning of June?**
-#' For now, let's do that.
+#' For now, let's do that. We also remove data from Olhos D'Agua Leste after the beginning of September.
 
+# first we do that for the original cleaned data
+mov.data.clean <- mov.data.clean %>% 
+  dplyr::filter(!(name == names[3] & timestamp > lubridate::ymd('2019-06-06')) & !(name == names[2] & timestamp > lubridate::ymd('2019-09-01')))
+
+# now we do the same for the track object
 mov.track <- mov.track %>% 
-  dplyr::filter(!(name == names[3] & t_ > lubridate::ymd('2019-06-06')))
+  dplyr::filter(!(name == names[3] & t_ > lubridate::ymd('2019-06-06')) & !(name == names[2] & t_ > lubridate::ymd('2019-09-01')))
 
 # replot
 # Monitoring period
@@ -276,20 +301,30 @@ g.tod <- ggplot() +
   ylim(0, 2)
 g.tod
 
-#' ## Are animals in a home range behavior?
+#' # Are animals in a home range behavior?
 #' 
-#' ### Method 1) Net square displacement
+#' Next step is to understand whether animals present a home range behavior, if they are still in 
+#' some kind of transient state to a ranging stability, or show some trace 
+#' that resembmles dispersal or nomadic movement. To do so, first we calculate and
+#' plot the net square displacement along time, for each individual, to keep track of how
+#' they move from their initial position, and if they seem to keep in the same areas.
+#' 
+#' 
+#' ## Method 1) Net square displacement
 #' 
 #' From the two analytical approaches below, it seems animals definetely present a
 #' ranging behavior. Maybe the animal from Alto Formoso still presents a bias towards long
-#' distance travels, but we're running `ctmm` analyses for all of them.
+#' distance travels, but we consider here this animal as ranging also. 
+#' Therefore we runn `ctmm` analyses for all of them, to calculate their areas of use based
+#' on their variograms, continuous time movement models, and the autocorrelated 
+#' kernel density estimator (AKDE).
 #' 
 
 # Calculate nsd
 mov.track.nsd <- mov.track.st %>% 
-  nest(-name) %>% 
-  mutate(nsd = map(data, function(x) sqrt((x$x_ - x$x_[1])**2 + (x$y_ - x$y_[1])**2))) %>%
-  unnest()
+  tidyr::nest(data = -name) %>% 
+  dplyr::mutate(nsd = map(data, function(x) sqrt((x$x_ - x$x_[1])**2 + (x$y_ - x$y_[1])**2))) %>%
+  tidyr::unnest(cols = c(data, nsd))
 
 g3 <- ggplot(mov.track.nsd) +
   geom_line(aes(x = t_, y = nsd/1000)) +
@@ -300,10 +335,23 @@ g3 <- ggplot(mov.track.nsd) +
   theme(axis.text.x = element_text(size = 12))
 g3
 
+#' Here we save all data loaded, just to be able to use it already organized in other 
+#' analyses.
 
+# save workspace
 save.image('data/movement_data_loaded.RData')
 
-#' ### Method 2) `ctmm` variograms
+#' ## Method 2) `ctmm` variograms
+#' 
+#' To run ctmm analyses, first we transform originalk cleaned data into a move object, from
+#' the package `move` and reproject it to the UTM 22S projection. Then we transform it into a 
+#' `telemetry` object so that it can be used for analyses in the `ctmm` package.
+#' 
+#' Next step is to plot the variograms for each individual, as a second way of checking whether the
+#' animals present a ranging behavior. Then we fit the continuous time movement models to the
+#' variograms, and use them to calculate the AKDE. Finally we plot them and also export the 
+#' 99% AKDE isopleths, as a basis for defining the area available to animals to understand 
+#' their resource selection behavior.
 
 # Transform data into move object
 mov.data.clean.df <- as.data.frame(mov.data.clean)
@@ -371,10 +419,10 @@ for(i in 1:length(mov.tel)) {
 }
 
 par(mfrow = c(2,2), mar = c(2, 2, 2, 1) + 0.1, oma = c(2,2,0,0))
-for(i in 1:length(mov.tel.res)) {
+for(i in 1:length(mov.tel)) {
   print(paste('individual', i, sep = ' = '))
   
-  ind <- mov.tel.res[[i]]
+  ind <- mov.tel[[i]]
   SVF <- variogram(ind)
   
   level <- c(0.5, 0.95) # 50% and 95% CIs
@@ -396,12 +444,12 @@ for(i in 1:length(mov.tel.res)) {
   # plot(krig)
 }
 mtext(expression(Semi-variance (km^2)), side = 2, line = 0.5, outer = T)
-mtext('Time between observations (meses)', side = 1, line = 1, outer = T,
+mtext('Time between observations', side = 1, line = 1, outer = T,
       at = 0.25)
 
-
-SpatialPolygonsDataFrame.akde(AKDE,alpha.HR=0.05)
-writeShapefile(AKDE.all[[1]], folder = "C:/Users/Ennio/Documents/Shapes AKDE 95", file = "polygontayassu_altoformoso", level.UD = 0.99)
-writeShapefile(AKDE.all[[2]], folder = "C:/Users/Ennio/Documents/Shapes AKDE 99", file = "polygontayassu_olhosleste", level.UD = 0.99)
-writeShapefile(AKDE.all[[3]], folder = "C:/Users/Ennio/Documents/Shapes AKDE 99", file = "polygontayassu_olhosoeste", level.UD = 0.99)
-writeShapefile(AKDE.all[[4]], folder = "C:/Users/Ennio/Documents/Shapes AKDE 99", file = "polygontayassu_pontal", level.UD = 0.99)
+# write 99% akde isopleths as shapefiles
+if(!dir.exists('maps/akde_99')) dir.create('maps/akde_99')
+writeShapefile(AKDE.all[[1]], folder = "maps/akde_99", file = "polygontayassu_altoformoso_akde99", level.UD = 0.99)
+writeShapefile(AKDE.all[[2]], folder = "maps/akde_99", file = "polygontayassu_olhosleste_akde99", level.UD = 0.99)
+writeShapefile(AKDE.all[[3]], folder = "maps/akde_99", file = "polygontayassu_olhosoeste_akde99", level.UD = 0.99)
+writeShapefile(AKDE.all[[4]], folder = "maps/akde_99", file = "polygontayassu_pontal_akde99", level.UD = 0.99)
--- code/movement_ssf.R
+++ code/movement_ssf.R
@@ -1,15 +1,21 @@
 #' ---
 #' title: 'White-lipped peccary movement in PN Emas - resource and step selection functions'
 #' author: Bernardo Niebuhr, Ennio Painkow, Ronaldo Morato - CENAP/ICMBio
+#' 
+#' abstract: "In this document we read land use data and perform habitat selection analyses
+#' for four white-lipped peccaries (__Tayassu pecari__) moving around the Emas National Park,
+#' in the Brazilian Cerrado."
 #' ---
 
-#' # Loading and organizing data
+#' # Loading movement and background data
 #' 
-#' First of all, we'll load and organize data, to prepare them as input to the
-#' different movement analysis packages. We're working here with data from four
-#' white-lipped peccary individuals (from four different groups) from the Parque Nacional
+#' First of all, we load the data that was already organized and cleaned in another step.
+#' We're working here with data from four white-lipped peccary individuals 
+#' (from four different groups) from the Parque Nacional
 #' das Emmas, within the Brazilian Cerrado.
-#' 
+#' We also load land use maps (that will be used here for the habitat selection analysis), 
+#' rasterize and merge them in two rasters: one with land use classes, and another with
+#' the date when the sugarcane and corn plantations were croped.
 
 # --------------- label=load_packages, warning=FALSE, message=FALSE, echo=FALSE
 
@@ -35,13 +41,14 @@ install.load::install_load('amt')
 
 # --------------- label=load_data
 
-#' ## Load data
+#' ## Load movement data
 #' 
 
 # Load movement data
 load('data/movement_data_loaded.RData')
 
-# Load spatial data
+#' ## Load spatial data
+#' 
 
 # land use classes
 lu.classes <- c('Campo Seco', 'Campo Úmido', 'Cana-de-Açúcar',
@@ -98,7 +105,7 @@ length(levels(map.olhos.oeste$classe))
 map.olhos.oeste$data.colheita[!is.na(map.olhos.oeste$data.colheita)]
 str(map.olhos.oeste)
 
-# olhos pontal
+# pontal
 map.pontal <- sf::st_read(dsn = 'maps/Pontal', layer = 'pontal') %>% 
   dplyr::rename(uso_terra = CLASS_A_19) %>% 
   dplyr::mutate(data.colheita = lubridate::ymd(DATA_COLHE) + days(1),
@@ -115,24 +122,29 @@ map.pontal$data.colheita[!is.na(map.pontal$data.colheita)]
 str(map.pontal)
 
 # rasterize
+
+# alto formoso
 r.af <- raster(map.alto.formoso, res = 10)
 rast.alto.formoso.class <- fasterize::fasterize(sf = map.alto.formoso, raster = r.af, field = 'classe.num')
 raster::plot(rast.alto.formoso.class)
 rast.alto.formoso.julian <- fasterize::fasterize(sf = map.alto.formoso, raster = r.af, field = 'julian')
 raster::plot(rast.alto.formoso.julian)
 
+# olhos leste
 r.ol <- raster(map.olhos.leste, res = 10)
 rast.olhos.leste.class <- fasterize::fasterize(sf = map.olhos.leste, raster = r.ol, field = 'classe.num')
 raster::plot(rast.olhos.leste.class)
 rast.olhos.leste.julian <- fasterize::fasterize(sf = map.olhos.leste, raster = r.ol, field = 'julian')
 raster::plot(rast.olhos.leste.julian)
 
+# olhos oeste
 r.oo <- raster(map.olhos.oeste, res = 10)
 rast.olhos.oeste.class <- fasterize::fasterize(sf = map.olhos.oeste, raster = r.oo, field = 'classe.num')
 raster::plot(rast.olhos.oeste.class)
 rast.olhos.oeste.julian <- fasterize::fasterize(sf = map.olhos.oeste, raster = r.oo, field = 'julian')
 raster::plot(rast.olhos.oeste.julian)
 
+# pontal
 r.po <- raster(map.pontal, res = 10)
 rast.pontal.class <- fasterize::fasterize(sf = map.pontal, raster = r.po, field = 'classe.num')
 raster::plot(rast.pontal.class)
@@ -160,6 +172,12 @@ maps <- raster::stack(rast.classes, rast.julian)
 names(maps) <- c('classes', 'julian.date')
 raster::plot(maps)
 
+#' # Step-selection functions for a single individual
+#' 
+#' Now we perform a step selection analysis for a single individual, just to get some 
+#' understanding of how it is responding to the habitat covariates. Then we do the same
+#' for all individuals 
+
 # --------------- label=ssf_one_individual
 
 coord.system <- as.character(maps@crs)
@@ -183,24 +201,16 @@ stps <- mov.track.1 %>%
 eda1 <- stps %>% 
   extract_covariates(maps, where = "start") %>% 
   mutate(classes.lev = factor(classes, levels = 1:length(lu.classes), labels = lu.classes),
-         julian.pos = julian(t1_),
-         classes.corr = as.factor(ifelse((classes.lev == lu.classes[3]) & (julian.pos > julian.date) & (julian.pos < julian.date + 3*30), 
-                                         lu.classes[7], as.character(classes.lev))),
-         classes.corr = as.factor(ifelse((classes.lev == lu.classes[6]) & (julian.pos > julian.date), 
-                                         lu.classes[7], as.character(classes.lev)))) %>% 
-  dplyr::filter(!is.na(classes.corr))
+         dttm.harvest = ymd('1970-01-01') + days(julian.date),
+         classes.corr = ifelse((classes.lev == lu.classes[3]) & (t1_ > dttm.harvest) & (t1_ < dttm.harvest + days(3*30)) & !is.na(dttm.harvest), 
+                               lu.classes[7], as.character(classes.lev)),
+         classes.corr = as.factor(ifelse((classes.corr == lu.classes[6]) & (t1_ > dttm.harvest) & !is.na(dttm.harvest), 
+                                         lu.classes[7], as.character(classes.corr))),
+         classes.corr = relevel(classes.corr, lu.classes[4])) %>% 
+  dplyr::filter(!is.na(classes.corr)) 
 
 ###
 ## Possibility: classificar o milho de maneira diferente apos a colheita; pode ser uma nova classe (durante um mes, e solo exposto apos)
-
-# Check
-eda1 %>% 
-  dplyr::filter(!is.na(julian.date)) %>% 
-  print(n = 200)
-
-eda1 %>% 
-  dplyr::filter(as.character(classes.lev) != as.character(classes.corr)) %>% 
-  print(n = 1000)
 
 # Some plots to explore
 
@@ -237,18 +247,40 @@ d1 <- stps %>%
   amt::random_steps(n = 9) %>% 
   extract_covariates(maps, where = "end") %>% 
   mutate(classes.lev = factor(classes, levels = 1:length(lu.classes), labels = lu.classes),
-         julian.pos = julian(t2_),
-         classes.corr = as.factor(ifelse((classes.lev == lu.classes[3]) & (julian.pos > julian.date) & (julian.pos < julian.date + 3*30), 
-                                         lu.classes[7], as.character(classes.lev))),
-         classes.corr = as.factor(ifelse((classes.lev == lu.classes[6]) & (julian.pos > julian.date), 
-                                         lu.classes[7], as.character(classes.lev)))) %>% 
-  dplyr::filter(!is.na(classes.corr)) %>% 
+         dttm.harvest = ymd('1970-01-01') + days(julian.date),
+         classes.corr = ifelse((classes.lev == lu.classes[3]) & (t2_ > dttm.harvest) & (t2_ < dttm.harvest + days(3*30)) & !is.na(dttm.harvest), 
+                                         lu.classes[7], as.character(classes.lev)),
+         classes.corr = as.factor(ifelse((classes.corr == lu.classes[6]) & (t2_ > dttm.harvest) & !is.na(dttm.harvest), 
+                                         lu.classes[7], as.character(classes.corr))),
+         classes.corr = relevel(classes.corr, lu.classes[4])) %>% 
+  dplyr::filter(!is.na(classes.corr)) %>%
   mutate(log_sl_ = log(sl_))
 
-# Check
-eda1 %>% 
+# plot
+
+par(mfrow = c(2,1))
+# used
+plot(maps$classes)
+points(stps$x2_, stps$y2_, pch = 19, col = grey(0.1, 0.5))
+plot(maps$classes)
+points(d1$x2_, d1$y2_, pch = 19, col = grey(0.1, 0.5))
+
+# VERIFICAAAAAAAAAAAAR!
+# clean steps for which the used or available steps go beyond the study area
+d1 %>% 
+  nest(-step_id_) %>% 
+  mutate(keep = ifelse(any(data)))
+
+# check
+d1 %>% 
   dplyr::filter(!is.na(julian.date)) %>% 
-  print(n = 200)
+  .[5500:6500,] %>% 
+  print(width = Inf, n = 1000)
+
+d1 %>% 
+  dplyr::filter(!is.na(julian.date)) %>% 
+  .[6500:7500,] %>% 
+  print(width = Inf, n = 1000)
 
 d1 %>% 
   dplyr::filter(as.character(classes.lev) != as.character(classes.corr)) %>% 
@@ -260,6 +292,14 @@ d1 %>%
 m1 <- d1 %>% 
   amt::fit_clogit(case_ ~ classes.corr + strata(step_id_))
 m1
+
+m2 <- d1 %>% 
+  amt::fit_clogit(case_ ~ classes.corr + log_sl_ + strata(step_id_))
+m2
+
+#install.load::install_load('ResourceSelection')
+# m1 <- ResourceSelection::rspf(case_ ~ classes.corr, data =  d1, m = ifelse(d1$case_, 1, 2))
+# m1
 
 AIC(m1$model)
 summary(m1)
@@ -280,19 +320,150 @@ m1.plot <- broom::tidy(m1$model) %>%
   coord_flip()
 m1.plot
 
+m2.plot <- broom::tidy(m2$model) %>% 
+  ggplot(aes(x = term, y = estimate)) +
+  geom_pointrange(aes(ymin = conf.low, ymax = conf.high),
+                  position = position_dodge(width = 0.7), size = 0.8) +
+  geom_hline(yintercept = 0, lty = 2) +
+  labs(y = "Relative Selection Strength") +
+  theme_light() + 
+  coord_flip()
+m2.plot
 
 
+# --------------- label=all_inds
+
+mov.track %>% 
+  nest(-name) %>% 
+  mutate(samp_rate = map(data, function(x) amt::summarize_sampling_rate(x))) %>% 
+  dplyr::select(name, samp_rate) %>% 
+  unnest
 
 
+mov.track.rsp.all <- mov.track %>% 
+  nest(-name) %>% 
+  mutate(trk = map(data, function(d) {
+    d %>% 
+      amt::track_resample(rate = minutes(30), tolerance = minutes(10)) %>%
+      amt::filter_min_n_burst(min_n = 3) %>% 
+      amt::transform_coords(crs_to = coord.system) %>% 
+      amt::steps_by_burst() %>% 
+      amt::random_steps(n = 9) %>% 
+      amt::extract_covariates(maps, when = 2) %>%
+      mutate(classes.lev = factor(classes, levels = 1:length(lu.classes), labels = lu.classes),
+             dttm.harvest = ymd('1970-01-01') + days(julian.date),
+             classes.corr = ifelse((classes.lev == lu.classes[3]) & (t2_ > dttm.harvest) & (t2_ < dttm.harvest + days(3*30)) & !is.na(dttm.harvest), 
+                                   lu.classes[7], as.character(classes.lev)),
+             classes.corr = as.factor(ifelse((classes.corr == lu.classes[6]) & (t2_ > dttm.harvest) & !is.na(dttm.harvest), 
+                                             lu.classes[7], as.character(classes.corr))),
+             classes.corr = relevel(classes.corr, lu.classes[4])) %>% 
+      dplyr::filter(!is.na(classes.corr)) %>%
+      mutate(log_sl_ = log(sl_))
+  }))
 
+# check
+mov.track.rsp.all %>% 
+  dplyr::select(-data) %>% 
+  unnest(cols = trk)
 
-mod.names <- c('m1', 'm2', 'm3', 'm4', 'm4.2', 'm5', 'm5.2', 'm6', 'm6.2', 'm7', 'm7.2')
-mods <- list(m1, m2, m3, m4, m4.2, m5, m5.2, m6, m6.2, m7, m7.2)
-(aic.100 <- tibble(model = mod.names,
-                   AIC = lapply(mods, function(x) x[[1]]) %>%
-                     lapply(., AIC) %>%
-                     unlist
-) %>%
-    dplyr::arrange(AIC) %>% 
-    mutate(dAIC = AIC - AIC[1],
-           wAIC = exp(-0.5*dAIC)/sum(exp(-0.5*dAIC))))
+mov.track.rsp.all %>% 
+  dplyr::select(-data) %>% 
+  unnest(cols = trk) %>% 
+  dplyr::filter(as.character(classes.lev) != as.character(classes.corr)) %>% 
+  print(n = 1000)
+  
+m1.all <- mov.track.rsp.all %>% 
+  mutate(fit = map(trk, ~ amt::fit_clogit(., case_ ~ classes.corr + strata(step_id_))))
+# 
+# m2.all <- mov.track.rsp.all %>% 
+#   mutate(fit = map(trk, ~ amt::fit_ssf(., case_ ~ prop_forest_100m + log_sl_ + log_dist_water + strata(step_id_))))
+# 
+# mod.names <- c('m1', 'm2', 'm3', 'm4', 'm4.2', 'm5', 'm5.2', 'm6', 'm6.2', 'm7', 'm7.2')
+# mods <- list(m1.all, m2.all, m3.all, m4.all, m4.2.all, m5.all, m5.2.all, m6.all, m6.2.all, m7.all, m7.2.all)
+# 
+# aics <- lapply(mods, function(y) y$fit %>%
+#                  lapply(., function(x) x[[1]]) %>%
+#                  lapply(., AIC) %>%
+#                  unlist
+# ) %>% 
+#   transpose() %>% 
+#   lapply(., function(x) do.call(rbind, x)) %>% 
+#   lapply(., function(x) {
+#     tibble(models = mod.names, AIC = x[,1]) %>% 
+#       dplyr::arrange(AIC) %>% 
+#       mutate(dAIC = AIC - AIC[1],
+#              wAIC = exp(-0.5*dAIC)/sum(exp(-0.5*dAIC)))
+#   })
+# names(aics) <- mov.track.rsp.all$name
+# aics
+
+# table with results
+d2 <- m1.all %>% 
+  mutate(coef = map(fit, ~ broom::tidy(.x$model))) %>% 
+  dplyr::select(name, coef) %>% 
+  unnest %>% 
+  mutate(name = factor(name),
+         term = factor(term))
+
+d2 %>% 
+  print(n = 50)
+
+d3 <- d2 %>% 
+  group_by(term) %>% 
+  summarize(mean = mean(estimate),
+            ymin = mean - 1.96 * sd(estimate)/sqrt(4),
+            ymax = mean + 1.96 * sd(estimate)/sqrt(4)) %>% 
+  dplyr::mutate(x = 1:n())
+
+p1 <- m1.all %>% 
+  mutate(coef = map(fit, ~ broom::tidy(.x$model))) %>%
+  dplyr::select(name, coef) %>% 
+  unnest %>% 
+  mutate(name = factor(name),
+         term = factor(term)) %>% 
+  ggplot(., aes(x = term, y = estimate, group = name, col = name)) + 
+  geom_rect(mapping = aes(xmin = x - .4, xmax = x + .4, ymin = ymin,
+                          ymax = ymax), data = d3, inherit.aes = FALSE, fill = "grey90") + 
+  geom_segment(mapping = aes(x = x - .4, xend = x + .4,
+                             y = mean, yend = mean), data = d3, inherit.aes = FALSE, size = 1) + 
+  geom_pointrange(aes(x = as.numeric(term), ymin = conf.low, ymax = conf.high), position = position_dodge(width = 0.7), size = 0.8) +
+  geom_hline(yintercept = 0, lty = 2) +
+  labs(x = "", y = "Relative Selection Strength") + 
+  theme_light() + 
+  coord_flip() +
+  geom_label(data = d3, aes(x = x, y = -4, label = term), inherit.aes = FALSE)
+p1
+ggsave('hab_selec_all_100m_ind.png', plot = p1, path = 'results', width = 30, height = 20, units = 'cm',
+       dpi = 300)
+
+p2 <- d3 %>% 
+  ggplot(., aes(x = term, y = mean)) + 
+  geom_pointrange(aes(ymin = ymin, ymax = ymax), position = position_dodge(width = 0.7), size = 0.8) +
+  geom_hline(yintercept = 0, lty = 2) +
+  labs(x = "", y = "Relative Selection Strength") + 
+  theme_light() + 
+  coord_flip()
+p2
+ggsave('hab_selec_all_100m_pop.png', plot = p2, path = 'results', width = 20, height = 20, units = 'cm',
+       dpi = 300)
+
+# for each individual
+m.each <- m1.all %>% 
+  mutate(coef = map(fit, ~ broom::tidy(.x$model)),
+         plots = map(coef, function(x) {
+           x %>% ggplot(aes(x = term, y = estimate)) +
+             geom_pointrange(aes(ymin = conf.low, ymax = conf.high),
+                             position = position_dodge(width = 0.7), size = 0.8) +
+             geom_hline(yintercept = 0, lty = 2) +
+             labs(y = "Relative Selection Strength") +
+             theme_light() + 
+             coord_flip()
+         })) %>% 
+  dplyr::select(name, plots)
+
+m.each$name
+
+m.each$plots[1]
+m.each$plots[2]
+m.each$plots[3]
+m.each$plots[4]
